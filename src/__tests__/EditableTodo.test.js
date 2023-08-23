2d6deec51f287e4dcdcf865e353b8da21ecda1b9
Jason Johnson <jasjoh@live.com>
2023-08-23 11:25:38 -0700

    mor tests

--- src/__tests__/EditableTodo.test.js
+++ src/__tests__/EditableTodo.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import EditableTodo from "../EditableTodo";
 import testTodos, { create, update, remove } from "./__testCommon";
 
@@ -10,7 +10,6 @@ describe("editable todo", function () {
       update={update}
       removeThis={remove} />);
   });
-
 
   it("renders correctly", function () {
     const { container, queryByText } = render(<EditableTodo todo={testTodos[0]}
@@ -24,4 +23,34 @@ describe("editable todo", function () {
     expect(delBtn).toBeInTheDocument();
     expect(editBtn).toBeInTheDocument();
   });
+
+  // test that if isEditing is true, renders the form
+
+  it("testing that edit shows form with correct data", function () {
+    const { container, queryByText, debug } = render(<EditableTodo todo={testTodos[0]}
+      update={update}
+      removeThis={remove} />);
+
+    const editBtn = container.querySelector(".EditableTodo-toggle");
+    fireEvent.click(editBtn);
+
+    debug(container);
+
+    // Go button exists
+    expect(container.querySelector('.NewTodoForm-addBtn')).toBeInTheDocument();
+
+    // Value of newTodo-title is 'Code!'
+    expect(
+      container.querySelector(`input[value="Code!"]`)
+    ).toBeInTheDocument();
+
+    // Value of newTodo-description is 'Write some code'
+    expect(queryByText("Write some code")).toBeInTheDocument();
+
+    // Value of priority selector is 2
+    const prioritySelector = container.querySelector('#newTodo-priority');
+    expect(prioritySelector.options.selectedIndex).toEqual(1)
+  });
+
+
 });
